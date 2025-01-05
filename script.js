4762e5648dcd11e59596bfe04f86107945306d1d
Humberto Garcia Jr <125167736+HumbertoGarciaJr@users.noreply.github.com>
2025-01-05 17:56:49 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//This is how you create an instance of a Rive Object
+//new rive.Rive({});
+
+// Initialize Rive
+// const riveInstance = new rive.Rive({
+//     src: "https://drive.google.com/file/d/1rneTnmRxJlKLGZ7zbwxruDjAC6Kvo1Jr/view?usp=sharing",
+//     canvas: document.getElementById("canvas"),
+//     autoplay: true,
+//     stateMachines: "Static",
+//     onLoad: () => {
+//         riveInstance.resizeDrawingSurfaceToCanvas();
+//         //console.log("Rive animation loaded successfully");  // Log when it's loaded
+//     }
+// });
+
+// artboard: "Arboard", // Optional. If not supplied the default is selected
+    // stateMachines: "bumpy",
+    // onLoad: () => {
+    //     r.resizeDrawingSurfaceToCanvas();
+    // },
